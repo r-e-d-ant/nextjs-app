bd811c54ea305278525c12e10a4fdba7f1aa1abf
r-e-d-ant <mugishathierry65@gmail.com>
2022-07-27 12:54:39 +0200

    cleaning

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+const About = () => {
+    return ( 
+        <div>
+            <Head>
+                <title>WebDev Newz - About</title>
+                <meta name='keywords' content='web development, programming' />
+              </Head>
+            <h1>About</h1>
+        </div>
+     );
+}
+ 
+export default About;
