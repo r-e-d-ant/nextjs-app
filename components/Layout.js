baf3c7abecb61e279d4b094e7b6f22ccb7111f47
r-e-d-ant <mugishathierry65@gmail.com>
2022-07-28 10:09:50 +0200

    nav

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,15 @@
 
 import styles from "../styles/Layout.module.css";
+import Nav from './Nav';
 
 const Layout = ({ children }) => {
     return ( 
+        <>
+        <Nav />
         <div className={styles.container}>
             <main className={styles.main}><h1>Hello</h1>{children}</main>
         </div>
+        </>
      );
 }
  
--- components/Nav.js
+++ components/Nav.js
@@ -1,8 +1,19 @@
+
+import Link from 'next/link';
+import navStyles from '../styles/Nav.module.css';
+
 const Nav = () => {
     return (
-        <div>
-            
-        </div>
+        <nav className={navStyles.nav}>
+            <ul>
+                <li>
+                    <Link href='/'>Home</Link>
+                </li>
+                <li>
+                    <Link href='/about'>About</Link>
+                </li>
+            </ul>
+        </nav>
     );
 }
  
